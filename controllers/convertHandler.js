d77b87912e9ab1189e96141e6e70ba39eb36617e
Caitlin <cmoconnor0823@gmail.com>
2020-08-10 11:30:05 -0700

    change in logic to meet fcc tests

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -6,74 +6,72 @@
 *       
 */
 
-//const validateInput = /([\d.s]+)([A-z]+\s*)/g;
-const units = {
-  lbs: 'kg',
-  kg: 'lbs',
-  mi: 'km',
-  km: 'mi',
-  gal: 'L',
-  L: 'gal',
-  lbs: 'kg',
-}
 
 
 function ConvertHandler() {
   
-  this.getNum = (input) => {
-    const unit = this.getUnit(input);
-    console.log(unit);
+  this.getNum = input => {
+  // Using regex find where the units begin in the user input 
+  const unitStartIndex = input.search(/[a-zA-z]/);
+  // Next create a variable to hold the input number
+  let inputNumbers;
 
-    if(unit === null){
-      return null;
-    }
-    const conversion = input.split(unit)[0];
-    console.log(conversion);
+  // Due to the requrements for the tests with chai we need to be able to retun "invalid unit"
+  // if no unit is present in the input. 
+  // This can be done with an if else where if unitStartIndex = -1 return "invalid unit"
 
-    try {
-      const result = eval(conversion === '' ? 1 : conversion);
-      return result;
-    } catch(e){
-      return null;
-    }
+  if(unitStartIndexs === -1) {
+    inputNumbers = input;
+    // if there is a unit present, we need to strip off the unit and leave only the numbers
+    // to be converted
+  } else {
+    inputNumbers = input.substring(0, unitStartIndexs);
+  }
+
+  // then a if check for a user story that states if a unit is present but no numbers
+  // assume a number input of one (1)
+
+  if (inputNumbers.length == 0) {
+    inputNumbers = "1"; // this needs to be saved as a string to match how we grabed the input numbers above
+  }
+
+  // now a check to cover the user story where we will have a invaild double fraction as an input
+  // remember at this point our inpitNumbers are a string
+  if(inputNumbers.split(/\//).length > 2) return "invalid number";
+
   };
   
   this.getUnit = input => {
-   
-    input.toLowerCase();
-    const regex = /(gal)|(l)|(lbs)|(kg)|(mi)|(km)|(GAL)|(L)|(LBS)|(KG)|(MI)|(KM)$/;
-    const result = input.match(regex);
+    // much in the same way as before we will start with finding where the units start in the input
+    // this time with a slight adjustment to account for spaces
+    let startUnitIndexs = input.search(/[\sa-zA-Z]/);
 
-    //console.log('Input Unit: ', result)
-    return result === null ? null : result[0];
+    // now that we have the indexs we can pull out a substring containing the entire unit
+    let units = input.substring(input.search(/[\sa-zA-Z]/), input.length);
+    // next we build an array of the acceptable inputs --note these are FCC specific---
+    let acceptableUnits = ['gal','l','mi','km','lbs','kg','GAL','L','MI','KM','LBS','KG'];
+      // now we create an array of the acceptable output units determined by FCC
+    let outputUnits = ['gal','l','mi','km','lbs','kg','gal','l','mi','km','lbs','kg',]
+
+    // now lets build a check to see if our input is acceptable
+    let acceptableInput = acceptableUnits.indexOf(units);
+
+    if ( acceptableInput >=0 ) {
+      return outputUnits[acceptableInput];
+    } else {
+      return "invalid unit";
+    }
   };
   
   this.getReturnUnit = initUnit => {
     if(initUnit === null){
-        return null;
+        return "invalid unit";
     }
     return units[initUnit];
   };
   
   this.spellOutUnit = unit => {
     
-    const validUnits = [
-      'gal',
-      'gallon',
-      'g',
-      'kg',
-      'killogram',
-      'kg',
-      'killograme',
-      'killometer',
-      'killometere',
-      'l',
-      'lb',
-      'litre',
-      'liter',
-      'mi',
-      'mile',
-      'pound']
     var result;
     
     return result;
